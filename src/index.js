11bb9454a4dbbc7ec9216e79067acf58a7c3e2a8
nati <nwtbekele@gmail.com>
2024-08-24 14:08:16 -0500

    some documentations for js functions

--- src/index.js
+++ src/index.js
@@ -25,13 +25,15 @@ document.addEventListener('mousemove', handleMouseMove);
 document.addEventListener('click', handleClick);
 document.addEventListener('DOMContentLoaded', handleWink);
 window.addEventListener('resize', resetCoords);
-window.addEventListener('scroll', resetCoords);
 
 // start wink handling
 async function handleWink() {
+  // Adds winking movement to the eyes. Randomly chooses and eye and also waits a random amount
+  // of time before the next wink
   setTimeout(
     () => {
       if (midAnimation) {
+        // make sure another animation is not in prograss
         handleWink();
         return;
       }
@@ -60,23 +62,27 @@ async function handleWink() {
       setTimeout(() => (midAnimation = false), 1000);
       handleWink();
     },
-    Math.round(Math.random() * 50000)
+    Math.round(Math.random() * 50000) // wait a random amount of time in the 50000 seconds
   );
 }
 // end wink handling
 
 // start click handling
 function handleClick(event) {
+  // Adds the ripple effect and if there are not other animations happening the eye brow
+  // movement
+
   const x = event.clientX + window.scrollX;
   const y = event.clientY + window.scrollY;
   createRipple(x, y);
   if (!midAnimation) {
     midAnimation = true;
-    setTimeout(moveBrows, 100);
+    setTimeout(moveBrows, 80);
   }
 }
 
 function createRipple(x, y) {
+  // Creates the ripple effect where a mouse press happens
   const ripple = document.createElement('div');
   ripple.classList.add('circle');
   ripple.style.left = `${x - 5}px`; // Adjusting position based on circle size
@@ -86,6 +92,7 @@ function createRipple(x, y) {
 }
 
 function moveBrows() {
+  // Adds brow movements by adding and removing the appropriate css animations
   [0, 1].forEach((idx) => {
     eyeBrows[idx].classList.add('raise-brow');
     eyeBrows[idx].addEventListener('animationend', () =>
@@ -102,6 +109,7 @@ function moveBrows() {
 
 // start updates when resizing the window
 function resetCoords() {
+  // Updates the positions of the eyeballs when the window is resized.
   eyeCoords = {
     leftx: eyeBalls[0].getBoundingClientRect().x,
     lefty: eyeBalls[0].getBoundingClientRect().y,
@@ -113,6 +121,8 @@ function resetCoords() {
 
 // start eye movement control
 function chooseEye(cursor_x) {
+  // Chooses the closest eyeball to the cursor. This eye will be used to calculate how much each
+  // eye should move rather than handling each as separate.
   left_dist = Math.abs(cursor_x - eyeCoords.leftx);
   right_dist = Math.abs(cursor_x - eyeCoords.rightx);
   if (left_dist <= right_dist) return eyeCoords.leftx;
@@ -120,12 +130,16 @@ function chooseEye(cursor_x) {
 }
 
 function lerpX(x, povX) {
+  // Linear interpolation in the x direction bounded by the BOUNDARYX (how far in the x direction
+  // the eye ball can move)
   const direction = (x - povX) / Math.abs(x - povX);
   const magnitude = x > povX ? x - povX : povX - x;
   return direction * Math.min((magnitude * BOUNDARYX) / povX, 35);
 }
 
 function lerpY(y) {
+  // Linear interpolation in the y direction bounded by the BOUNDARYY (how far in the y direction
+  // the eye ball can move)
   // boundary of a 50 rad circle
   const direction = (y - eyeCoords.lefty) / Math.abs(y - eyeCoords.lefty);
   const magnitude = y > eyeCoords.lefty ? y - eyeCoords.lefty : eyeCoords.lefty - y;
@@ -133,6 +147,9 @@ function lerpY(y) {
 }
 
 function handleMouseMove(event) {
+  // Handles the mouse movement events. If the cursor is inbetween the eyes, the only movement is
+  // in the y direction. Otherwise, the closest eye is chosen and the eye balls are moved to follow
+  // the cursor from the POV of the chosen eye.
   x = event.clientX;
   y = event.clientY;
   let dy = lerpY(y);
